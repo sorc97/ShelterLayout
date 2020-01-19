4f41e52f509e4f9ec942a000d4ecd0a6cf201a5e
sorc97 <nnd64809200@gmail.com>
2020-01-19 22:28:14 +0300

    modal window

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 'use strict'
 
-// Slider variables
+// Slider elements
 let sliderItems = document.querySelectorAll(".slider__item");
 let sliderItem = document.querySelector(".slider__item");
 let sliderList = document.querySelector(".slider__list");
 let leftArrow = document.querySelector(".slider__button_arrow-left");
 let rightArrow = document.querySelector(".slider__button_arrow-right");
-
+// Modal elements
+let petButtons = document.querySelectorAll(".friends-item__button");
+// Slider variables
 let translateValue = 0;
 let visibleItems = Math.round(sliderList.offsetWidth/sliderItem.offsetWidth);
 let currentItem = visibleItems;
@@ -31,3 +33,42 @@ rightArrow.addEventListener('click', slideToRight);
 
 // Modal methods
 
+petButtons.forEach(button => button.addEventListener('click', toggleModal));
+
+class Modal {
+  constructor(content) {
+    const modalWrapper = document.createElement('div');
+    modalWrapper.className = 'modal-window';
+    modalWrapper.innerHTML = `
+      <div class='modal-window__content'>
+        ${content}
+        <button class="modal-window__button modal-window__button_close action-button fa fa-close" onclick="toggleModal()">
+        </button>
+      </div>
+    `;
+
+    return modalWrapper;
+  }
+}
+
+function toggleModal(e) {
+  const modal = document.querySelector('.modal-window');
+  // If modal does not exist, create it
+  if(!modal) {
+    document.body.append(new Modal(getModalContent(e)));
+    return;
+  }
+  // If modal already exist, toggle his visibility
+  /* const modalVisibility = modal.style.display;
+  modal.style.display = (modalVisibility === 'none') ? 'flex' : 'none'; */
+  modal.remove();
+}
+
+function getModalContent(e) {
+  let target = e.target;
+  console.log(target);
+  let modalItem = target.parentNode.querySelector('.modal-item');
+  // let content = modalItem.firstElementChild;
+
+  return modalItem.innerHTML;
+}
